b243aad35846c04a7c5b8cc2a4ce324c34dc7e3e
Lisa <llavia@MacBook-Pro.local>
2021-03-13 12:04:21 -0500

    first commit

---
+++ num-obj-method.js
@@ -0,0 +1,77 @@
+//place answer below each challenge. Be sure to test your results in the  colsole.
+
+//Hint, you can find examples on the slides!
+
+//Example: Convert the integer 321 into a string
+//Example answer: 
+let x  = 321;
+let y = x.toString();
+console.log(y)
+//Example result:“321”
+
+// ------------------------------------------------------------- 
+//1: Convert 10 into a string
+//1 Answer:
+let j = 10;
+let k = j.toString();
+console.log(k)
+//1 Result:
+result = 10
+
+//2: Convert 1400 into a string
+//2 Answer:
+let a = 1400;
+let b = a.toString();
+console.log(b);
+//2 Result:
+1400
+
+//3: Convert 103456 into a string
+//3 Answer:
+let c = 103456;
+let d = c.toString();
+console.log(d);
+//3 Result:
+103456
+
+//4: Convert the boolean true and false into integers
+//4 Answer:
+let z = Number(true);
+let y = Number(false)
+
+console.log (Number(z));
+//4 Result:
+1   // 1 means its true 
+
+//4: Convert the boolean true and false into integers
+//4 Answer:
+let w = Number(true);
+let x = Number(false)
+
+console.log (Number(x));
+
+//4 Result:
+0 //0 means false
+
+//5: Convert new Date into integers
+//5 Answer:
+let newDate = Number(10);
+console.log(newDate); 
+//5 Result:
+10
+
+//6: Convert the string "02130" into an integer
+//6 Answer:
+let e = "02130";
+let d = Number(e);
+console.log(d);
+//6 Result:
+02130
+//7: Use "03 1010" string as a value in a variable in an attempt to convert into an integer. Be sure to include the quotations.
+//7 Answer:
+let r = "03 1010";
+let t = Number(r);
+console.log(t);
+
+//7 Result:
+NaN
